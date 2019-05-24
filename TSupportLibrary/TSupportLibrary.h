0aaa98230353b3f601d303490b00dc033786d870
matteocorradin <maxcorrads@gmail.com>
2019-05-24 20:21:35 +0200

    Initial Commit

---
+++ TSupportLibrary/TSupportLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TSupportLibrary.h
+//  TSupportLibrary
+//
+//  Created by Matteo Corradin on 24/05/2019.
+//  Copyright © 2019 Matteo Corradin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSupportLibrary.
+FOUNDATION_EXPORT double TSupportLibraryVersionNumber;
+
+//! Project version string for TSupportLibrary.
+FOUNDATION_EXPORT const unsigned char TSupportLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSupportLibrary/PublicHeader.h>
+
+
